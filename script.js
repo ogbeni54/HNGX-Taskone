506e713dfba44a406d053e6229ea6cfaf63bedb7
ogbeni54 <58536139+ogbeni54@users.noreply.github.com>
2023-09-07 13:34:55 +0100

    feat: Making the time to update without refreshing the page

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const UTC= document.querySelector('.time_of_the_week');
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date();
 const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
-const currentUtcTime = new Date().toUTCString();
+
 
 
 
 day.textContent = currentDayOfWeek;
-UTC.textContent = currentUtcTime
+
+
+const updateUtcTime = () => {
+    const currentUtcTime = new Date().toUTCString();
+    UTC.textContent = currentUtcTime;
+}
+
+setInterval(() => {
+    updateUtcTime();
+}, 1000);
+
+updateUtcTime();
+
 
 console.log(day.innerHTML);
