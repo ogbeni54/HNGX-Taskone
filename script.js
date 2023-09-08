d920b291b79e3d104e9d9a150459d6f0850af4df
ogbeni54 <58536139+ogbeni54@users.noreply.github.com>
2023-09-08 12:34:40 +0100

    feat: fixing time of the week

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const day= document.querySelector('.day_of_week');
 const UTC= document.querySelector('.time_of_the_week');
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDate = Date.now();
-// const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
-const currentDayOfWeek = currentDate
+const currentDatey = Date.now();
+const currentDate = new Date();
+const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+const currentTime = currentDatey
 
 
 
+
+UTC.textContent = currentTime;
 
 day.textContent = currentDayOfWeek;
+// const updateUtcTime = () => {
+//     const currentUtcTime = new Date().toUTCString();
+//     UTC.textContent = currentUtcTime;
+// }
 
+// setInterval(() => {
+//     updateUtcTime();
+// }, 1000);
 
-const updateUtcTime = () => {
-    const currentUtcTime = new Date().toUTCString();
-    UTC.textContent = currentUtcTime;
-}
-
-setInterval(() => {
-    updateUtcTime();
-}, 1000);
-
-updateUtcTime();
+// updateUtcTime();
 
 
 console.log(day.innerHTML);
