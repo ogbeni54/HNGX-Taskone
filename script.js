798f70acd4562baa8886ceb95ebb91c51e55eb59
ogbeni54 <58536139+ogbeni54@users.noreply.github.com>
2023-09-08 12:18:50 +0100

    feat: fixing of day of week

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const day= document.querySelector('.day_of_week');
 const UTC= document.querySelector('.time_of_the_week');
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDate = new Date();
-const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+const currentDate = Date.now();
+// const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+const currentDayOfWeek = currentDate
 
 
 
